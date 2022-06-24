94b170de269a47c21630c11be8b2273d1c34fac0
Joshua Coffey <josh@trinsic.id>
2022-06-23 21:25:40 -0400

    Add .NET support

--- build_test.ps1
+++ build_test.ps1
@@ -3,11 +3,12 @@ go build
 
 $RenamePairs = "trust-registry=trustregistry,universal-wallet=wallet,verifiable-credentials=credential,templates=template"
 $PythonPath = "python_path=../sdk/python/trinsic"
+$DotnetPath = "dotnet_path=../sdk/dotnet/Trinsic"
 $DartPath ="dart_path=../sdk/dart/lib/src"
 $GolangPath = "golang_path=../sdk/go/services"
 $TypescriptPath = "typescript_path=../sdk/web/src"
 
 protoc --proto_path=..\sdk\proto `
-       --trinsic-sdk_out="${RenamePairs},${DartPath},${PythonPath},${GolangPath},${TypescriptPath}:" `
+       --trinsic-sdk_out="${RenamePairs},${DartPath},${PythonPath},${GolangPath},${TypescriptPath},${DotnetPath}:" `
        --plugin="protoc-gen-trinsic-sdk=${PSScriptRoot}/protoc-gen-sdk.exe" `
         ..\sdk\proto\services\account\v1\*.proto ..\sdk\proto\services\provider\v1\*.proto ..\sdk\proto\services\verifiable-credentials\v1\*.proto ..\sdk\proto\services\verifiable-credentials\templates\v1\*.proto ..\sdk\proto\services\trust-registry\v1\*.proto ..\sdk\proto\services\universal-wallet\v1\*.proto
--- main.go
+++ main.go
@@ -15,6 +15,7 @@ type trinsicModule struct {
 	serviceTpl *template.Template
 	fileExt    string
 	targetName string
+	fileSuffix string
 }
 
 func trinsicDart() *trinsicModule {
@@ -24,6 +25,7 @@ func trinsicDart() *trinsicModule {
 		serviceTpl: template.Must(template.New("dartService").Funcs(funcs).Parse(lang_types.DartServiceTpl)),
 		fileExt:    "dart",
 		targetName: "dart_path",
+		fileSuffix: "_service",
 	}
 }
 
@@ -34,6 +36,7 @@ func trinsicPython() *trinsicModule {
 		serviceTpl: template.Must(template.New("pythonService").Funcs(funcs).Parse(lang_types.PythonServiceTpl)),
 		fileExt:    "py",
 		targetName: "python_path",
+		fileSuffix: "_service",
 	}
 }
 
@@ -44,6 +47,7 @@ func trinsicGolangInterface() *trinsicModule {
 		serviceTpl: template.Must(template.New("golangServiceInterface").Funcs(funcs).Parse(lang_types.GoServiceInterfaceTpl)),
 		fileExt:    "go",
 		targetName: "golang_path",
+		fileSuffix: "_service",
 	}
 }
 
@@ -54,12 +58,25 @@ func trinsicGolangImplementation() *trinsicModule {
 		serviceTpl: template.Must(template.New("golangServiceImplementation").Funcs(funcs).Parse(lang_types.GoServiceImplTpl)),
 		fileExt:    "go",
 		targetName: "golang_path",
+		fileSuffix: "_service",
+	}
+}
+
+func trinsicDotnet() *trinsicModule {
+	funcs := getTemplateFuncs()
+	return &trinsicModule{
+		ModuleBase: &pgs.ModuleBase{},
+		serviceTpl: template.Must(template.New("dotnetService").Funcs(funcs).Parse(lang_types.DotnetServiceTpl)),
+		fileExt:    "cs",
+		targetName: "dotnet_path",
+		fileSuffix: "Service",
 	}
 }
 
 func getTemplateFuncs() map[string]interface{} {
 	funcs := map[string]interface{}{
 		"MethodParamType":        lang_types.MethodParamType,
+		"MethodIsStreaming":      lang_types.MethodIsStreaming,
 		"DartMethodReturnType":   lang_types.DartMethodReturnType,
 		"DartDocComment":         lang_types.DartEntityDocComment,
 		"DartAsync":              lang_types.DartAsync,
@@ -71,6 +88,9 @@ func getTemplateFuncs() map[string]interface{} {
 		"GolangMethodParamType":  lang_types.GoMethodParamType,
 		"GolangStructPointer":    lang_types.GoStructPointer,
 		"GolangStructPointerVar": lang_types.GolangStructPointerVar,
+		"DotnetMethodReturnType": lang_types.DotnetMethodReturnType,
+		"DotnetDocComment":       lang_types.DotnetEntityDocComment,
+		"DotnetMethodParamType":  lang_types.DotnetMethodParamType,
 	}
 	return funcs
 }
@@ -108,7 +128,7 @@ func (t *trinsicSdk) TargetPath() string {
 	baseName := t.File().InputPath().BaseName()
 	// Handle argument renaming
 	targetName := t.module.Parameters().StrDefault(baseName, baseName)
-	targetFile := t.module.JoinPath(t.module.Parameters().StrDefault(t.module.targetName, ""), targetName+"_service."+t.module.fileExt)
+	targetFile := t.module.JoinPath(t.module.Parameters().StrDefault(t.module.targetName, ""), targetName+t.module.fileSuffix+"."+t.module.fileExt)
 	// Handle ":" drive on windows
 	targetFile = strings.Replace(targetFile, "?", ":", 1)
 	return targetFile
@@ -135,6 +155,7 @@ func main() {
 		RegisterModule(trinsicPython()).
 		RegisterModule(trinsicGolangInterface()).
 		RegisterModule(trinsicGolangImplementation()).
+		RegisterModule(trinsicDotnet()).
 		RegisterPostProcessor(applyTemplateFiles()).
 		Render()
 }
