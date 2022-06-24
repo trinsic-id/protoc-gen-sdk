9852fe8acb6ffd71cfd98891298c079b5047f86d
Scott Phillips <polygonguru@gmail.com>
2022-06-23 20:37:10 -0400

    Clean up (#1)

--- build_test.ps1
+++ build_test.ps1
@@ -2,11 +2,12 @@ Set-Location $PSScriptRoot
 go build
 
 $RenamePairs = "trust-registry=trustregistry,universal-wallet=wallet,verifiable-credentials=credential,templates=template"
-$DartPath = "python_path=../sdk/python/trinsic"
-$PythonPath ="dart_path=../sdk/dart/lib/src"
+$PythonPath = "python_path=../sdk/python/trinsic"
+$DartPath ="dart_path=../sdk/dart/lib/src"
 $GolangPath = "golang_path=../sdk/go/services"
+$TypescriptPath = "typescript_path=../sdk/web/src"
 
 protoc --proto_path=..\sdk\proto `
-       --trinsic-sdk_out="${RenamePairs},${DartPath},${PythonPath},${GolangPath}:" `
+       --trinsic-sdk_out="${RenamePairs},${DartPath},${PythonPath},${GolangPath},${TypescriptPath}:" `
        --plugin="protoc-gen-trinsic-sdk=${PSScriptRoot}/protoc-gen-sdk.exe" `
         ..\sdk\proto\services\account\v1\*.proto ..\sdk\proto\services\provider\v1\*.proto ..\sdk\proto\services\verifiable-credentials\v1\*.proto ..\sdk\proto\services\verifiable-credentials\templates\v1\*.proto ..\sdk\proto\services\trust-registry\v1\*.proto ..\sdk\proto\services\universal-wallet\v1\*.proto
