c77f46dfa70d63fd2a1c66e7da64c1e47d3376f6
Scott Phillips <polygonguru@gmail.com>
2022-08-26 19:09:50 -0400

    Convert this into a github action (#19)

---
+++ action.ps1
@@ -0,0 +1,36 @@
+param(
+    [Parameter(Mandatory=$true)][string]$ProtoPath,
+    [Parameter(Mandatory=$true)][string]$RenamePairs,
+    [Parameter(Mandatory=$true)][string]$PythonPath,
+    [Parameter(Mandatory=$true)][string]$DotnetPath,
+    [Parameter(Mandatory=$true)][string]$DartPath,
+    [Parameter(Mandatory=$true)][string]$GolangPath,
+    [Parameter(Mandatory=$true)][string]$TypescriptPath,
+    [Parameter(Mandatory=$true)][string]$JavaKotlinPath,
+    [Parameter(Mandatory=$true)][string]$RubyPath,
+    [Parameter(Mandatory=$true)][string]$SwiftPath
+)
+
+Set-Location $PSScriptRoot
+
+go build
+
+$PROTO_DIR = Resolve-Path "$PSScriptRoot/$ProtoPath"
+
+$PythonArg = "python_path=${PythonPath}"
+$DotnetArg = "dotnet_path=${DotnetPath}"
+$DartArg = "dart_path=${DartPath}"
+$GolangArg = "golang_path=${GolangPath}"
+$TypescriptArg = "typescript_path=${TypescriptPath}"
+$JavaKotlinArg = "javakotlin_path=${JavaKotlinPath}"
+$RubyArg = "ruby_path=${RubyPath}"
+$SwiftArg = "swift_path=${SwiftPath}"
+
+
+foreach ($Item in Get-ChildItem -Path $PROTO_DIR -Include *.proto -Recurse)
+{
+    $File = $Item.FullName
+    $Expr = "protoc --plugin=protoc-gen-trinsic-sdk=${PSScriptRoot}/protoc-gen-sdk.exe --trinsic-sdk_out=${RenamePairs},${DartArg},${PythonArg},${GolangArg},${TypescriptArg},${DotnetArg},${JavaKotlinArg},${RubyArg},${SwiftArg}: -I $PROTO_DIR $File"
+    Write-Output $Expr
+    Invoke-Expression $Expr
+}
