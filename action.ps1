61e1c1e14e4cd9f144189b6e04180553655ebf7a
Scott Phillips <polygonguru@gmail.com>
2022-08-30 14:39:20 -0400

    Use the precompiled action with only necessary binaries (#22)

--- action.ps1
+++ action.ps1
@@ -1,23 +1,17 @@
-param(
-    [Parameter(Mandatory=$true)][string]$ProtoPath,
-    [Parameter(Mandatory=$true)][string]$RenamePairs,
-    [Parameter(Mandatory=$true)][string]$PythonPath,
-    [Parameter(Mandatory=$true)][string]$DotnetPath,
-    [Parameter(Mandatory=$true)][string]$DartPath,
-    [Parameter(Mandatory=$true)][string]$GolangPath,
-    [Parameter(Mandatory=$true)][string]$TypescriptPath,
-    [Parameter(Mandatory=$true)][string]$JavaKotlinPath,
-    [Parameter(Mandatory=$true)][string]$RubyPath,
-    [Parameter(Mandatory=$true)][string]$SwiftPath
-)
-
-go version
-go mod vendor
-go build
+param([Parameter(Mandatory = $true)][string]$ProtoPath, [Parameter(Mandatory = $true)][string]$RenamePairs, [Parameter(Mandatory = $true)][string]$PythonPath, [Parameter(Mandatory = $true)][string]$DotnetPath, [Parameter(Mandatory = $true)][string]$DartPath, [Parameter(Mandatory = $true)][string]$GolangPath, [Parameter(Mandatory = $true)][string]$TypescriptPath, [Parameter(Mandatory = $true)][string]$JavaKotlinPath, [Parameter(Mandatory = $true)][string]$RubyPath, [Parameter(Mandatory = $true)][string]$SwiftPath)
 
 Set-Location $PSScriptRoot
 
-$PROTO_DIR = Resolve-Path "$PSScriptRoot/$ProtoPath"
+$ProtoPath = (Resolve-Path $ProtoPath).Path
+$PythonPath = (Resolve-Path $PythonPath).Path.Replace(":","?")
+$DartPath = (Resolve-Path $DartPath).Path.Replace(":","?")
+$DotnetPath = (Resolve-Path $DotnetPath).Path.Replace(":","?")
+$GolangPath = (Resolve-Path $GolangPath).Path.Replace(":","?")
+$TypescriptPath = (Resolve-Path $TypescriptPath).Path.Replace(":","?")
+$JavaKotlinPath = (Resolve-Path $JavaKotlinPath).Path.Replace(":","?")
+$RubyPath = (Resolve-Path $RubyPath).Path.Replace(":","?")
+$SwiftPath = (Resolve-Path $SwiftPath).Path.Replace(":","?")
+
 
 $PythonArg = "python_path=${PythonPath}"
 $DotnetArg = "dotnet_path=${DotnetPath}"
@@ -28,12 +22,33 @@ $JavaKotlinArg = "javakotlin_path=${JavaKotlinPath}"
 $RubyArg = "ruby_path=${RubyPath}"
 $SwiftArg = "swift_path=${SwiftPath}"
 
-$PluginPath = "${PSScriptRoot}/protoc-gen-sdk$(If ($IsWindows) {'.exe'} Else {''})"
+# TODO - Support ARM64 identification
+$PluginPath = Resolve-Path "${PSScriptRoot}/go-plugin/protoc-gen-sdk-$( If ($IsWindows)
+{
+    'windows'
+}
+ElseIf ($IsLinux)
+{
+    'linux'
+}
+ElseIf ($IsMacOS)
+{
+    'darwin'
+} )-amd64$( If ($IsWindows)
+{
+    '.exe'
+}
+Else
+{
+    ''
+} )"
 
-foreach ($Item in Get-ChildItem -Path $PROTO_DIR -Include *.proto -Recurse)
+Write-Output $PluginPath
+
+foreach ($Item in Get-ChildItem -Path $ProtoPath -Include *.proto -Recurse)
 {
     $File = $Item.FullName
-    $Expr = "protoc --plugin=protoc-gen-trinsic-sdk=${PluginPath} --trinsic-sdk_out=${RenamePairs},${DartArg},${PythonArg},${GolangArg},${TypescriptArg},${DotnetArg},${JavaKotlinArg},${RubyArg},${SwiftArg}: -I $PROTO_DIR $File"
+    $Expr = "protoc --plugin=protoc-gen-trinsic-sdk=${PluginPath} --trinsic-sdk_out=${RenamePairs},${DartArg},${PythonArg},${GolangArg},${TypescriptArg},${DotnetArg},${JavaKotlinArg},${RubyArg},${SwiftArg}: -I $ProtoPath $File"
     Write-Output $Expr
     Invoke-Expression $Expr
 }
